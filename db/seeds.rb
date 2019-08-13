7320680f35bf37310a2d9b9078aa62b745c96f5d
Brandon Rachal <brandon.rachal@gmail.com>
2019-08-13 14:20:27 -0500

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+load_files = ['testers.csv', 'devices.csv', 'bugs.csv', 'tester_device.csv']
+load_files.each do |file_name|
+	csv_path = File.join(__dir__, file_name)
+	klass_name = File.basename(file_name, ".*").classify
+	klass = klass_name.constantize
+	id_name = "#{klass_name.underscore}_id"
+	CSV.foreach(csv_path, :headers => true) do |row|
+		row_id, row_data = nil, {}
+	  row.each do |key, value|
+	  	rails_key = key.underscore
+	  	if rails_key == id_name
+	  		row_id = value
+	  	else
+	  		row_data[rails_key] = value
+	  	end
+	  end
+	  new_record = klass.new(row_data)
+	  new_record.id = row_id if row_id
+	 	new_record.save!
+	end
+end
