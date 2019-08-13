5d0afb30e4c29f70883e8fa2b7ba4c8b1dcd9d22
Brandon Rachal <brandon.rachal@gmail.com>
2019-08-13 13:18:24 -0500

    Test validations for description on Device.

--- test/models/device_test.rb
+++ test/models/device_test.rb
@@ -2,8 +2,28 @@ require 'test_helper'
 
 class DeviceTest < ActiveSupport::TestCase
 
+	def setup
+		@device = devices(:galaxy_s10)
+	end
+
 	test 'should be an instance of Device' do
-		assert_instance_of Device, Device.new
+		assert_instance_of Device, @device
+	end
+
+	test 'should be valid' do
+		assert @device.valid?
+	end
+
+	test 'should be invalid with a blank description' do
+		@device.description = ''
+		refute @device.valid?, 'is valid with a blank description'
+		assert_not_nil @device.errors[:description], 'no validation for blank description'
+	end
+
+	test 'should be invalid with a description over 255 chars' do
+		@device.description = 'a' * 256
+		refute @device.valid?, 'is valid with description over 255 chars'
+		assert_not_nil @device.errors[:description], 'no validation for description over 255 chars'
 	end
 
 end
